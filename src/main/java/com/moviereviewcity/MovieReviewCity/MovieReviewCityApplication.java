2b6c32e3ff1b64276756366b321c9f3b6f273fa9
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-05-15 14:47:58 -0400

    Added web-starter dependency and index.html landing page

--- src/main/java/com/moviereviewcity/MovieReviewCity/MovieReviewCityApplication.java
+++ src/main/java/com/moviereviewcity/MovieReviewCity/MovieReviewCityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieReviewCityApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MovieReviewCityApplication.class, args);
 	}
 
