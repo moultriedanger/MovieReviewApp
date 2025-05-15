c2f9759f41855dac0295d3c103d02f39bc6172ab
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-05-15 14:15:59 -0400

    inital commit

---
+++ src/main/java/com/moviereviewcity/MovieReviewCity/MovieReviewCityApplication.java
@@ -0,0 +1,13 @@
+package com.moviereviewcity.MovieReviewCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviereviewcity/MovieReviewCity/MovieReviewCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviereviewcity.MovieReviewCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
